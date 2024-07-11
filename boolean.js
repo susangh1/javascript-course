b6144875f6a611105b700df6195a4989bd01d3fc
Susan Ghimire <susgh39@gmail.com>
2024-07-11 16:21:41 +0545

    Initial commit

---
+++ boolean.js
@@ -0,0 +1,19 @@
+console.log(typeof true);
+
+console.log(3 > 5 - 5);
+
+if (false) {
+  console.log("hello");
+} else {
+  console.log("else");
+}
+const age = 25;
+
+if (age >= 16) {
+  console.log("you can drive");
+  console.log("congrats");
+} else if (age >= 14) {
+  console.log("almost there");
+} else {
+  console.log("you cant drive");
+}
---
+++ cart.js
@@ -0,0 +1,5 @@
+let cartQuantity = 0;
+const variable2 = "hello";
+var variable3 = 3;
+
+console.log(typeof variable2);
